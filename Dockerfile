40d5d064bff901aaad638ba861b93d65bb79670a
xfyygvcghyttt <sadeghkiri@atomicmail.io>
2026-09-03 18:16:26 +0330

    Add files via upload

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+﻿FROM alpine:3.20
+
+RUN apk add --no-cache dante-server
+
+COPY sockd.conf /etc/sockd.conf
+COPY start.sh /start.sh
+
+RUN chmod +x /start.sh
+
+EXPOSE 1080
+
+CMD ["/start.sh"]
---
+++ start.sh
@@ -0,0 +1,12 @@
+﻿#!/bin/sh
+
+set -e
+
+: "${SOCKS_USER:?SOCKS_USER is required}"
+: "${SOCKS_PASSWORD:?SOCKS_PASSWORD is required}"
+
+adduser -D "$SOCKS_USER"
+
+echo "$SOCKS_USER:$SOCKS_PASSWORD" | chpasswd
+
+exec sockd -f /etc/sockd.conf
